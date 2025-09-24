5f1d9b19f11e70ddb033d4abaf1799564ed0dfea
joelsinnott <joelsinnott@hotmail.com>
2025-09-24 16:29:55 +0200

    let the robot do its thing

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,10 @@
 export default function(eleventyConfig) {
+  const urlFilter = (path) => eleventyConfig.getFilter("url")(path);
   eleventyConfig.addPassthroughCopy({ "assets": "assets" });
   eleventyConfig.addShortcode("figure", (src, alt = "", caption = "") => {
     return `
   <figure class="my-6">
-    <img src="${src}" alt="${alt}" class="rounded">
+    <img src="${urlFilter(src)}" alt="${alt}" class="rounded">
     ${caption ? `<figcaption class="mt-2 text-sm text-base-600">${caption}</figcaption>` : ""}
   </figure>`;
   });
@@ -15,7 +16,8 @@ export default function(eleventyConfig) {
       output: "_site"
     },
     htmlTemplateEngine: "liquid",
-    markdownTemplateEngine: "liquid"
+    markdownTemplateEngine: "liquid",
+    pathPrefix: process.env.PATH_PREFIX || "/"
   };
 }
 
